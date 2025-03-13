f3024ff03473e3c431de343e9d5e2f75ccc79015
Javohir5006 <joker55855585@gmail.com>
2025-03-13 13:35:19 +0500

    Update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "../styles/contact.css";
 import Footer from "../components/footer";
-import location from "../assets/img/image1.jpg";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -26,7 +25,9 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert(`Ваше сообщение отправлено:\nEmail: ${formData.email}\nТелефон: ${formData.phone}\nСообщение: ${formData.message}`);
+    alert(
+      `Ваше сообщение отправлено:\nEmail: ${formData.email}\nТелефон: ${formData.phone}\nСообщение: ${formData.message}`
+    );
   };
 
   return (
@@ -64,8 +65,15 @@ const Contact = () => {
         </div>
 
         {/* ✅ Карта */}
-        <div className="map-section" data-aos="fade-left">
-          <img src={location} alt="Карта" />
+        <div className="map-container" data-aos="fade-left">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31213.812659339387!2d64.39238765!3d39.767586549999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f50063f4cba7e6f%3A0x7fd1d742b00b154e!2z0JHRg9GA0L7QvdGB0LrQsNGPINGD0LsuLCAyMDAwMA!5e0!3m2!1sru!2s!4v1710330305907!5m2!1sru!2s"
+            width="450"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
         </div>
       </section>
 
